e3351e00d9fa7bd2280fe1b3ea2f65d2bff076ee
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-04-03 18:53:37 +0530

    feat: add util function

--- code/backend/src/utils/index.ts
+++ code/backend/src/utils/index.ts
@@ -1,1 +1,24 @@
-export const handleAPICall = async (companyName: string, categoryName: string) => { };
+import config from "../config";
+
+const baseURL = config.baseUrl;
+const authToken = config.token.secret;
+
+export const handleAPICall = async (companyName: string, categoryName: string, maxPrice: number, minPrice: number, top: number) => {
+    const apiUrl = `${baseURL}/test/companies/${companyName}/categories/${categoryName}/products?top=${top}&minPrice=${minPrice}&maxPrice=${maxPrice}`;
+
+    const headers = {
+        Authorization: `Bearer ${authToken}`
+    };
+
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: headers
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+};
